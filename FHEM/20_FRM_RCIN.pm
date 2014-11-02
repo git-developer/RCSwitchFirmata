60d8d3e460d82b6e79f8e5ae43f24fcd76edad86
git-developer <git-developer@users.noreply.github.com>
2014-11-02 12:09:23 +0100

    02.11.2014 (2)

--- FHEM/20_FRM_RCIN.pm
+++ FHEM/20_FRM_RCIN.pm
@@ -1,56 +1,60 @@
-#############################################
 package main;
 
 use strict;
 use warnings;
 
-#####################################
+our ($readingFnAttributes, %attr);
 
 use constant {
-  PINMODE_RCINPUT  => 11,
+  PINMODE_RCINPUT   => 11,
 
-  RCINPUT_TOLERANCE             => 0x31,
-  RCINPUT_RAW_DATA              => 0x32,
-  RCINPUT_MESSAGE               => 0x41,
+  RCINPUT_TOLERANCE => 0x31,
+  RCINPUT_RAW_DATA  => 0x32,
+  RCINPUT_MESSAGE   => 0x41,
 };
 
-my %rcswitchAttributes = (
-  "tolerance"        => RCINPUT_TOLERANCE,
-  "rawDataEnabled"   => RCINPUT_RAW_DATA,
-);
-
-my %moduleAttributes = (
-);
+use constant RCIN_PARAMETERS => {
+  tolerance => RCINPUT_TOLERANCE,
+  rawData   => RCINPUT_RAW_DATA,
+};
+use constant RCIN_PARAMETER_NAMES => { reverse(%{RCIN_PARAMETERS()}) };
 
 sub
 FRM_RCIN_Initialize($)
 {
   my ($hash) = @_;
 
-  $hash->{DefFn}     = "FRM_Client_Define";
-  $hash->{UndefFn}   = "FRM_Client_Undef";
-  $hash->{InitFn}    = "FRM_RCIN_Init";
-  $hash->{AttrFn}    = "FRM_RCIN_Attr";
+  $hash->{DefFn}     = 'FRM_RC_Define';
+  $hash->{UndefFn}   = 'FRM_RC_Undefine';
+  $hash->{InitFn}    = 'FRM_RCIN_Init';
+  $hash->{NotifyFn}  = 'FRM_RCIN_Notify';
+  $hash->{AttrFn}    = 'FRM_RCIN_Attr';
   
-  LoadModule("FRM_RC");
+  LoadModule('FRM_RC');
 
-  $hash->{AttrList}  = "IODev"
-                       . " " . join(" ", keys %rcswitchAttributes)
-                       . " " . join(" ", keys %main::rcAttributes)
-                       . " " . $main::readingFnAttributes;
+  $hash->{AttrList} = join(' ', FRM_RCIN_get_attributes(),
+                                keys %{RC_ATTRIBUTES()},
+                                $readingFnAttributes);
 }
 
 sub
 FRM_RCIN_Init($$)
 {
   my ($hash, $args) = @_;
-  FRM_RC_Init($hash, PINMODE_RCINPUT, \&FRM_RCIN_handle_rc_response, \%rcswitchAttributes, \%moduleAttributes, $args);
+  FRM_RC_Init($hash, PINMODE_RCINPUT, \&FRM_RCIN_handle_rc_response, $args);
+}
+
+sub FRM_RCIN_Notify {
+  my ($hash, $dev) = @_;
+  return FRM_RC_Notify($hash, $dev, RCIN_PARAMETERS);
 }
 
 sub
 FRM_RCIN_Attr($$$$) {
   my ($command, $name, $attribute, $value) = @_;
-  return FRM_RC_Attr($command, $name, $attribute, $value, \%rcswitchAttributes);
+  return FRM_RC_Attr($command, $name, $attribute,
+                     FRM_RCIN_get_internal_value($attribute, $value),
+                     RCIN_PARAMETERS);
 }
 
 sub FRM_RCIN_handle_rc_response {
@@ -73,29 +77,27 @@ sub FRM_RCIN_handle_rc_response {
       push @data, (shift @data) + ((shift @data) << 8);
   }
 
-  FRM_RCIN_observer($command, \@data, $hash);
+  FRM_RCIN_notify($hash, $command, \@data);
 }
 
-sub FRM_RCIN_observer
+sub FRM_RCIN_notify
 {
-  my ( $key, $value, $hash ) = @_;
+  my ($hash, $key, $value) = @_;
   my $name = $hash->{NAME};
+  my $attrName = RCIN_PARAMETER_NAMES->{$key};
   
-  my %a = reverse(%rcswitchAttributes);
-  my $attrName = $a{$key};
-  
-COMMAND_HANDLER: {
+  COMMAND_HANDLER: {
     ($key eq RCINPUT_MESSAGE) and do {
 
       my ($longCode, $bitCount, $delay, $protocol, $tristateCode, $rawData) = @$value;
-      my $rawInt = join(" ", @$rawData);
-      my $rawHex = join(" ", map { sprintf "%04X", $_ } @$rawData);
+      my $rawInt = join(' ', @$rawData);
+      my $rawHex = join(' ', map { sprintf "%04X", $_ } @$rawData);
 
-      Log3($hash, 4, "message: " . join(", ", @$value));
-      my $verboseLevel = $main::attr{$name}{"verbose"};
-      if (defined $verboseLevel and $verboseLevel > 3) {
+      Log3($hash, 4, 'message: ' . join(', ', @$value));
+      my $verboseLevel = $attr{$name}{'verbose'};
+      if (defined $verboseLevel and $verboseLevel > 3 and defined $rawHex and $rawHex) {
         my $s = $rawHex;
-        my $rawBlock = "";
+        my $rawBlock = '';
         while ($s) {
           $rawBlock .= substr($s, 0, 40, '')."\n";
         }
@@ -108,21 +110,21 @@ COMMAND_HANDLER: {
       readingsBulkUpdate($hash, 'bitCount', $bitCount);
       readingsBulkUpdate($hash, 'delay', $delay);
       readingsBulkUpdate($hash, 'protocol', $protocol);
-      if (defined $main::attr{$name}{'rawDataEnabled'} and $main::attr{$name}{'rawDataEnabled'} ne 0) {
+      if (FRM_RCIN_is_rawdata_enabled($name)) {
         readingsBulkUpdate($hash, 'rawData', $rawHex);
       }
       readingsEndUpdate($hash, 1);
       last;
     };
     defined($attrName) and do {
-      $value = shift @$value;
-      Log3($name, 4, "$attrName: $value");
-
-      $main::attr{$name}{$attrName}=$value;
+      $value = FRM_RCIN_get_user_value($attrName, shift @$value);
+      Log3($hash, 4, "$attrName: $value");
+    
+      $attr{$name}{$attrName}=$value;
       # TODO refresh web GUI somehow?
       last;
     };
-};
+  };
 }
 
 sub FRM_RCIN_long_to_tristate_code {
@@ -135,10 +137,37 @@ sub FRM_RCIN_long_to_tristate_code {
   return $tristateCode;
 }
 
+sub FRM_RCIN_get_attributes {
+  return map({$_ eq 'rawData' ? $_ . ':enabled,disabled': $_}
+             keys %{RCIN_PARAMETERS()});
+}
+
+sub FRM_RCIN_get_internal_value($$) {
+  my ($attribute, $value) = @_;
+  if ($attribute eq 'rawData') {
+    $value = $value eq 'enabled' ? 1 : 0;
+  }
+  return $value;
+}
+
+sub FRM_RCIN_get_user_value($$) {
+  my ($attribute, $value) = @_;
+  if ($attribute eq 'rawData') {
+    $value = $value ? 'enabled' : 'disabled'; 
+  }
+  return $value;
+}
+
+sub FRM_RCIN_is_rawdata_enabled($) {
+  my $name = shift;
+  my $rawData = $attr{$name}{'rawData'};
+  return defined $rawData and $rawData eq 'enabled';	
+}
 
 1;
 
 =pod
+
 =begin html
 
 <a name="FRM_RCIN"></a>
@@ -146,14 +175,13 @@ sub FRM_RCIN_long_to_tristate_code {
   <p>
    Represents a pin of an <a href="http://www.arduino.cc">Arduino</a> running
    <a href="http://www.firmata.org">Firmata</a> configured to receive data via
-   the RCSwitch library.<br />
+   the <a href="https://code.google.com/p/rc-switch/">RCSwitch</a> library.<br />
    Requires a defined <a href="#FRM">FRM</a>-device to work.<br><br> 
   </p>
   <a name="FRM_RCINdefine"></a>
   <h4>Define</h4>
-   <p>
-    <code>define &lt;name&gt; FRM_RCIN &lt;pin&gt;</code> <br/>
-    Defines the FRM_RCIN device. &lt;pin&gt> is the arduino-pin to use.
+   <p><pre>    define &lt;name&gt; FRM_RCIN &lt;pin&gt;</pre> <br/>
+    Defines the FRM_RCIN device. <code>&lt;pin&gt</code> is the arduino-pin to use.
    </p>
   <a name="FRM_RCINset"></a>
   <h4>Set</h4>
@@ -168,20 +196,22 @@ sub FRM_RCIN_long_to_tristate_code {
   <a name="FRM_RCINattr"></a>
   <h4>Attributes</h4>
   <ul>
-    <li><a href="#IODev">IODev</a><br/>
-      Specify which <a href="#FRM">FRM</a> to use.
+    <li>
+     <a href="#IODev">IODev</a>: specify which <a href="#FRM">FRM</a> device
+     to use.
     </li>
     <li>
       <code>tolerance</code>: RCSwitch parameter <code>receiveTolerance</code> in percent
-      (default: 60; see RCSwitch for details)
+      (default: 60; see RCSwitch documentation for details)
     </li>
     <li>
-      <code>rawDataEnabled</code>: If set to 1, an additional reading
+      <code>rawData</code>: If set to <code>enabled</code>, an additional reading
       <code>rawData</code> will be created, containing the received data in raw
-      format (default: 0 which means that reporting of raw data is disabled)
+      format (default: <code>disabled</code>)
     </li>
-    <li><a href="#eventMap">eventMap</a><br></li>
-    <li><a href="#readingFnAttributes">readingFnAttributes</a><br></li>
+    <li><a href="#FRM_RCattr">FRM_RC attributes</a></li>
+    <li><a href="#eventMap">eventMap</a></li>
+    <li><a href="#readingFnAttributes">readingFnAttributes</a></li>
     </ul>
 <br>
 
--- FHEM/20_FRM_RCOUT.pm
+++ FHEM/20_FRM_RCOUT.pm
@@ -1,10 +1,9 @@
-#############################################
 package main;
 
 use strict;
 use warnings;
 
-#####################################
+our ($readingFnAttributes, %attr);
 
 use constant {
   PINMODE_RCOUTPUT              => 10,
@@ -19,78 +18,88 @@ use constant {
   RCOUTPUT_CODE_PACKED_TRISTATE => 0x28,
 };
 
+use constant RCOUT_GETS             => { 
+  raw => '',
+};
 
-my %gets = (
-  "raw" => 1,
-);
+use constant RCOUT_SETS             => {
+  tristateCode     => RCOUTPUT_CODE_PACKED_TRISTATE,
+  longCode         => RCOUTPUT_CODE_LONG,
+  charCode         => RCOUTPUT_CODE_CHAR,
+};
+use constant RCOUT_SET_NAMES        => { 
+  reverse(%{RCOUT_SETS()}),
+};
 
-my %sets = (
-  "tristateCode"     => RCOUTPUT_CODE_PACKED_TRISTATE,
-  "longCode"         => RCOUTPUT_CODE_LONG,
-  "charCode"         => RCOUTPUT_CODE_CHAR,
-);
+use constant RCOUT_PARAMETERS       => {
+  protocol         => RCOUTPUT_PROTOCOL,
+  pulseLength      => RCOUTPUT_PULSE_LENGTH,
+  repeatTransmit   => RCOUTPUT_REPEAT_TRANSMIT,
+};
+use constant RCOUT_PARAMETER_NAMES  => { 
+  reverse(%{RCOUT_PARAMETERS()}),
+};
 
-my %rcswitchAttributes = (
-  "protocol"         => RCOUTPUT_PROTOCOL,
-  "pulseLength"      => RCOUTPUT_PULSE_LENGTH,
-  "repeatTransmit"   => RCOUTPUT_REPEAT_TRANSMIT,
-);
-
-my %moduleAttributes = (
+use constant RCOUT_ATTRIBUTES       => {
   defaultBitCount  => 24,
-);
+};
 
-my @clients = qw( IT );
+use constant CLIENTS                => qw( IT );
+
 
 sub
 FRM_RCOUT_Initialize($)
 {
   my ($hash) = @_;
 
-  $hash->{DefFn}     = "FRM_Client_Define";
-  $hash->{UndefFn}   = "FRM_Client_Undef";
-  $hash->{InitFn}    = "FRM_RCOUT_Init";
-  $hash->{AttrFn}    = "FRM_RCOUT_Attr";
-  $hash->{GetFn}     = "FRM_RCOUT_Get";
-  $hash->{SetFn}     = "FRM_RCOUT_Set";
- 
-  LoadModule("FRM_RC");
-  
-  $hash->{AttrList}  = "IODev"
-                         . " " . join(" ", keys %rcswitchAttributes)
-                         . " " . join(" ", keys %moduleAttributes)
-                         . " " . join(" ", keys %main::rcAttributes)
-                         . " " . $main::readingFnAttributes;
+  $hash->{DefFn}     = 'FRM_RC_Define';
+  $hash->{UndefFn}   = 'FRM_RC_Undefine';
+  $hash->{InitFn}    = 'FRM_RCOUT_Init';
+  $hash->{NotifyFn}  = 'FRM_RCOUT_Notify';
+  $hash->{AttrFn}    = 'FRM_RCOUT_Attr';
+  $hash->{GetFn}     = 'FRM_RCOUT_Get';
+  $hash->{SetFn}     = 'FRM_RCOUT_Set'; 
 
-  $hash->{Clients} = join (':', @clients);
+  LoadModule('FRM_RC');
+
+  $hash->{AttrList} = join(' ', keys %{RCOUT_PARAMETERS()},
+                                keys %{RC_ATTRIBUTES()},
+                                keys %{RCOUT_ATTRIBUTES()},
+                                $readingFnAttributes);
+  $hash->{Clients}  = join (':', CLIENTS);
 }
 
 sub
 FRM_RCOUT_Init($$)
 {
   my ($hash, $args) = @_;
-  FRM_RC_Init($hash, PINMODE_RCOUTPUT, \&FRM_RCOUT_handle_rc_response, \%rcswitchAttributes, \%moduleAttributes, $args);
+  FRM_RC_Init($hash, PINMODE_RCOUTPUT, \&FRM_RCOUT_handle_rc_response, $args);
+}
+
+sub FRM_RCOUT_Notify {
+  my ($hash, $dev) = @_;
+  return FRM_RC_Notify($hash, $dev, RCOUT_PARAMETERS);
 }
 
 sub
 FRM_RCOUT_Attr($$$$)
 {
   my ($command, $name, $attribute, $value) = @_;
-  return FRM_RC_Attr($command, $name, $attribute, $value, \%rcswitchAttributes);
+  return FRM_RC_Attr($command, $name, $attribute, $value, RCOUT_PARAMETERS);
 }
 
 # FRM_RCOUT_Get behaves as CUL_Get so that 10_IT can use FRM_RCOUT as IODev
 sub
 FRM_RCOUT_Get($@)
 {
-  my ($self, $name, $get, $codeCommand) = @_;
+  my ($hash, $name, $get, $codeCommand) = @_;
 
-  if(!defined($get) or !defined($gets{$get})) {
+  if (!defined($get) or !defined(RCOUT_GETS->{$get})) {
     return undef;
   }
 
   my ($code) = $codeCommand =~ /is([01fF]+)/;
-  my $set = FRM_RCOUT_Set($self, $self->{NAME}, "tristateCode", $code);
+  my $set = FRM_RCOUT_Set($hash, $hash->{NAME}, 'tristateCode', $code);
   return "raw => $codeCommand";
 }
 
@@ -98,10 +107,15 @@ sub
 FRM_RCOUT_Set($@)
 {
   my ($hash, @a) = @_;
-  return "Need at least 2 parameters" if(@a < 2);
-  my $command = $sets{$a[1]};
-  return "Unknown argument $a[1], choose one of " . join(" ", sort keys %sets)
-    if(!defined($command));
+  return 'Need at least 2 parameters' if(@a < 2);
+  my $command = RCOUT_SETS->{$a[1]};
+  if (!defined($command)) {
+	  return "Unknown argument $a[1], choose one of "
+	           . join(' ', sort keys %{RCOUT_SETS()})
+  }
+  if (!defined $a[2]) {
+    return "Second parameter is undefined: $hash, " . join(", ", @a);
+  }
   my @code;
   eval {
     if ($command eq RCOUTPUT_CODE_PACKED_TRISTATE) {
@@ -109,13 +123,13 @@ FRM_RCOUT_Set($@)
     } elsif ($command eq RCOUTPUT_CODE_LONG) {
       my $value = $a[2];
       my $bitCount = $a[3];
-      $bitCount = $main::attr{$hash->{NAME}}{defaultBitCount} if not defined $bitCount;
-      $bitCount = $moduleAttributes{defaultBitCount} if not defined $bitCount; # if defaultBitCount was deleted
+      $bitCount = $attr{$hash->{NAME}}{defaultBitCount} if not defined $bitCount;
+      $bitCount = RCOUT_ATTRIBUTES->{defaultBitCount} if not defined $bitCount; # if defaultBitCount was deleted
       @code = ($bitCount, $value);
     } elsif ($command eq RCOUTPUT_CODE_CHAR) {
-        @code = map {ord($_)} split("", $a[2]);
+        @code = map {ord($_)} split('', $a[2]);
     }
-     FRM_RCOUT_send_code(FRM_Client_FirmataDevice($hash), $command, $hash->{PIN}, @code);
+     FRM_RCOUT_send_code($hash, $command, $hash->{PIN}, @code);
   };
   return $@;
 }
@@ -141,70 +155,65 @@ sub FRM_RCOUT_handle_rc_response {
                + ((shift @data) << 16) + ((shift @data) << 24);
 
   } elsif ($command eq RCOUTPUT_CODE_CHAR) {
-    my $charCode = join("", map { chr($_); } @data);
+    my $charCode = join('', map { chr($_); } @data);
     @data = ($charCode);
   } else { # parameter as int
       push @data, (shift @data) + ((shift @data) << 8);
   }
   
-  FRM_RCOUT_observer($command, \@data, $hash);
+  FRM_RCOUT_notify($hash, $command, \@data);
 }
 
-sub FRM_RCOUT_observer
+sub FRM_RCOUT_notify
 {
-  my ( $key, $data, $hash ) = @_;
+  my ($hash, $key, $data) = @_;
   my $name = $hash->{NAME};
+  my $subcommand  = RCOUT_SET_NAMES->{$key};
+  my $attrName    = RCOUT_PARAMETER_NAMES->{$key};
   
-  my %s = reverse(%sets);
-  my %a = reverse(%rcswitchAttributes);
-  my $subcommand = $s{$key};
-  my $attrName = $a{$key};
-  
-COMMAND_HANDLER: {
+  COMMAND_HANDLER: {
     defined($subcommand) and do {
-      if ("tristateCode" eq $subcommand) {
+      if ('tristateCode' eq $subcommand) {
         my $tristateCode = shift @$data;
-        Log3($name, 4, "$subcommand: $tristateCode");
+        Log3($hash, 4, "$subcommand: $tristateCode");
         readingsSingleUpdate($hash, $subcommand, $tristateCode, 1);
-      } elsif ("longCode" eq $subcommand) {
+      } elsif ('longCode' eq $subcommand) {
         my $bitCount = shift @$data;
         my $longCode  = shift @$data;
-        Log3($name, 4, "$subcommand: $longCode/$bitCount");
+        Log3($hash, 4, "$subcommand: $longCode/$bitCount");
         readingsBeginUpdate($hash);
         readingsBulkUpdate($hash, $subcommand, $longCode);
-        readingsBulkUpdate($hash, "bitCount", $bitCount);
+        readingsBulkUpdate($hash, 'bitCount', $bitCount);
         readingsEndUpdate($hash, 1);
-      } elsif ("charCode" eq $subcommand || "tristateString" eq $subcommand) {
+      } elsif ('charCode' eq $subcommand) {
         my $charCode = shift @$data; 
-        Log3($name, 4, "$subcommand: $charCode");
+        Log3($hash, 4, "$subcommand: $charCode");
         readingsSingleUpdate($hash, $subcommand, $charCode, 1);
       } else {
-        readingsSingleUpdate($hash, "state", "unknown subcommand $subcommand", 1);
+        readingsSingleUpdate($hash, 'state', "unknown subcommand $subcommand", 1);
       }
       last;
     };
     defined($attrName) and do {
       my $value = shift @$data;
-      Log3($name, 4, "$attrName: $value");
+      Log3($hash, 4, "$attrName: $value");
 
-      $main::attr{$name}{$attrName}=$value;
+      $attr{$name}{$attrName}=$value;
       # TODO refresh web GUI somehow?
       last;
     };
-};
+  };
 }
 
 sub FRM_RCOUT_send_code {
-  my ( $firmata, $subcommand, $pin, @code ) = @_;
-  my $protocol = $firmata->{protocol};
-main::Log3("sender", 3, "pin $pin: " . join(",", @code));  
+  my ( $hash, $subcommand, $pin, @code ) = @_;
   my @transferCode = ();
   if ($subcommand eq RCOUTPUT_CODE_PACKED_TRISTATE) {
   
     # @code is a list of tristate bits.
     # 4 tristate bits per byte will be sent to the microcontroller;
     # the last byte has to be filled up with value-less data
-    my @transferSymbols = FRM_RC_align(@code);
+    my @transferSymbols = FRM_RC_get_tristate_byte(@code);
 
     # pack each 4 tristate bits into 1 byte
     for (my $i = 0; $i < @transferSymbols; $i++) {
@@ -230,7 +239,7 @@ main::Log3("sender", 3, "pin $pin: " . join(",", @code));
     die "Unsupported subcommand $subcommand";
   }
   
-  return FRM_RC_send_message($firmata, $subcommand, $pin, @transferCode);
+  return FRM_RC_send_message($hash,  $subcommand, $pin, @transferCode);
 }
 
 # extract tristate bit from byte (containing 4 tristate bits)
@@ -254,32 +263,35 @@ sub FRM_RCOUT_set_tristate_bit {
 1;
 
 =pod
+
 =begin html
 
 <a name="FRM_RCOUT"></a>
 <h3>FRM_RCOUT</h3>
   <p>
     Represents a pin of an <a href="http://www.arduino.cc">Arduino</a> running
-    <a href="http://www.firmata.org">Firmata</a> configured to send data via the
-    RCSwitch library.<br/>
+    <a href="http://www.firmata.org">Firmata</a> configured to send data via
+    the <a href="https://code.google.com/p/rc-switch/">RCSwitch</a> library.
+    <br/>
     Requires a defined <a href="#FRM">FRM</a>-device to work.
   </p>
   <a name="FRM_RCOUTdefine" />
   <h4>Define</h4>
   <p>
-    <code>define &lt;name&gt; FRM_RCOUT &lt;pin&gt;</code><br/>
-    Defines the FRM_RCOUT device. &lt;pin&gt; is the arduino-pin to use.
+    <pre>    define &lt;name&gt; FRM_RCOUT &lt;pin&gt;</pre> <br />
+    Defines the FRM_RCOUT device. <code>&lt;pin&gt;</code> is the arduino-pin to
+    use.
   </p>
   <a name="FRM_RCOUTset" />
   <h4>Set</h4>
   <ul>
    <li>
      <code>set &lt;name&gt; tristateCode &lt;code&gt;</code><br/>
-     Sends a tristate code, e.g. <code>00F0FF0FFF0F<code>
+     Sends a tristate code, e.g. <code>00F0FF0FFF0F</code>
    </li>
    <li>
-     <code>set &lt;name&gt; longCode &lt;code&gt; &lt;bitCount&gt;</code><br/>
-     Sends a code as long decimal, e.g. <code>282961 24<code>.
+     <code>set &lt;name&gt; longCode &lt;code&gt; &lt;bitCount&gt;</code> <br />
+     Sends a code as long decimal, e.g. <code>282961 24</code>.
      The first argument gives a bit pattern as long (32 bits),
      the second argument gives the number of bits that will actually be sent.
      For example, <code>5 8</code> will be sent as <code>0000 0101</code>
@@ -290,38 +302,54 @@ sub FRM_RCOUT_set_tristate_bit {
    </li>
    <li>
      <code>set &lt;name&gt; charCode &lt;code&gt;</code><br/>
-     sends a character code, e.g. <code>001011011101<code>
+     sends a character code, e.g. <code>001011011101</code>
    </li>
   </ul>
+
   <a name="FRM_RCOUTget" />
   <h4>Get</h4>
-    N/A
-  <br/>
+  <ul>
+   <li>
+     <code>get &lt;name&gt; raw is&lt;code&gt;</code> <br />
+     Sends a tristate code, e.g. <code>00F0FF0FFF0F</code>. This <em>get</em>
+     is for compatibility to the <a href="#CUL">CUL</a> module. It allows a
+     FRM_RCOUT device to be used as IODev for an <a href="#IT">IT</a> device.
+     Example:
+     <pre>
+       define firmata FRM COM9@57600
+       define sender FRM_RCOUT 4
+       attr sender IODev firmata
+       define switch IT 00F0FF0FFF 0F 00
+       attr switch IODev sender
+     </pre>
+   </li>
+  </ul>
+
   <a name="FRM_RCOUTattr" />
   <h4>Attributes</h4>
   <ul>
     <li>
-      <a href="#IODev">IODev</a><br/>
-      Specify which <a href="#FRM">FRM</a> to use.
+      <a href="#IODev">IODev</a>: specify which <a href="#FRM">FRM</a> to use.
     </li>
     <li>
       <code>protocol</code>: RCSwitch parameter <code>protocol</code>
-      (default: 1; see RCSwitch for details)
+      (default: 1; see RCSwitch documentation for details)
     </li>
     <li>
-      <code>pulseLength: RCSwitch parameter <code>pulseLength</code>
-      (default: 350; see RCSwitch for details)</code>
+      <code>pulseLength</code>: RCSwitch parameter <code>pulseLength</code>
+      (default: 350; see RCSwitch documentation for details)</code>
     </li>
     <li>
-      <code>repeatTransmit: RCSwitch parameter <code>repeatTransmit</code>
-      (default: 10; see RCSwitch for details)</code>
+      <code>repeatTransmit</code>: RCSwitch parameter <code>repeatTransmit</code>
+      (default: 10; see RCSwitch documentation for details)</code>
     </li>
     <li>
-      <code>defaultBitCount: Default for the <code>bitCount</code> parameter
+      <code>defaultBitCount</code>: Default for the <code>bitCount</code> parameter
       of the <code>longCode</code> command (default: 24) 
     </li>
-    <li><a href="#eventMap">eventMap</a><br></li>
-    <li><a href="#readingFnAttributes">readingFnAttributes</a><br></li>
+    <li><a href="#FRM_RCattr">FRM_RC attributes</a></li>
+    <li><a href="#eventMap">eventMap</a></li>
+    <li><a href="#readingFnAttributes">readingFnAttributes</a></li>
   </ul>
 
 =end html
